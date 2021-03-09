9f722d37d164727147962c4a5ded13bd13261f13
YunKihyeon <ykh094@naver.com>
2021-03-09 15:19:24 +0900

    create Spring REST API server

---
+++ backend/src/main/java/com/ssafy/gambti/GambtiApplication.java
@@ -0,0 +1,13 @@
+package com.ssafy.gambti;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GambtiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GambtiApplication.class, args);
+    }
+
+}
