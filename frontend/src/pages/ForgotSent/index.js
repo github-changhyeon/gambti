b21bb338560de9040fa4658119233526c69eea90
백민주 <baiktte@gmail.com>
2021-03-12 18:14:50 +0900

    S04P22B201-52 / feat : 공통컴포넌트 ButtonComp 생성

--- frontend/src/pages/ForgotSent/index.js
+++ frontend/src/pages/ForgotSent/index.js
@@ -1,16 +1,23 @@
 import React from 'react';
 import styles from './index.module.css';
 import AvatarComp from 'src/components/AvatarComp/AvatarComp';
+import ButtonComp from 'src/components/ButtonComp/ButtonComp';
 
 
 export default function ForgotSent() {
-  const [size, setSize] = React.useState('small')
-  const [textvalue, setTextvalue] = React.useState('Hi')
+  // AvatarComp 사이즈, textvalue
+  const [asize, setAsize] = React.useState('small')
+  const [atextvalue, setAtextvalue] = React.useState('Hi')
+  // ButtonComp 사이즈, textvalue
+  const [bsize, setBsize] = React.useState('sm')
+  const [btextvalue, setBtextvalue] = React.useState('Hi')
 
   return (
     <div>
       <h1>Hello ForgotSent</h1>
-      <AvatarComp size={size} textvalue={textvalue}></AvatarComp>
+      {/* AvatarComp 사용할때 size랑 textvalue를 줘야해 */}
+      {/* <AvatarComp size={asize} textvalue={atextvalue}></AvatarComp> */}
+      <ButtonComp size={bsize} textvalue={btextvalue}></ButtonComp>
     </div >
   );
 }
