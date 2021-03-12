8b4074f396c04ff010448b446c59bda3224ed21e
백민주 <baiktte@gmail.com>
2021-03-12 17:19:37 +0900

    S04P22B201-50 / feat : 공통컴포넌트 Avatar 생성

--- frontend/src/pages/ForgotSent/index.js
+++ frontend/src/pages/ForgotSent/index.js
@@ -1,6 +1,16 @@
 import React from 'react';
 import styles from './index.module.css';
+import AvatarComp from 'src/components/AvatarComp/AvatarComp';
+
 
 export default function ForgotSent() {
-  return <h1>Hello ForgotSent</h1>;
+  const [size, setSize] = React.useState('small')
+  const [textvalue, setTextvalue] = React.useState('Hi')
+
+  return (
+    <div>
+      <h1>Hello ForgotSent</h1>
+      <AvatarComp size={size} textvalue={textvalue}></AvatarComp>
+    </div >
+  );
 }
