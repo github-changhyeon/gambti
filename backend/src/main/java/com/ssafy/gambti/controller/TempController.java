17c1a3cdb173afa2f61f548a6a81637da25552aa
YunKihyeon <ykh094@naver.com>
2021-03-15 16:13:15 +0900

    S04P22B201-60 / feat: firebase token을 이용한 security 설정

--- backend/src/main/java/com/ssafy/gambti/controller/TempController.java
+++ backend/src/main/java/com/ssafy/gambti/controller/TempController.java
@@ -1,4 +1,23 @@
 package com.ssafy.gambti.controller;
 
+import com.ssafy.gambti.models.basicResponse.Response;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Tag(name="temp", description = "temp API")
+@RestController
+@RequestMapping(value = "/temp")
 public class TempController {
+    private static final String SUCCESS = "success";
+
+    @GetMapping("/test")
+    public ResponseEntity<? extends Response> temp(){
+
+
+        return new  ResponseEntity<>(new Response(SUCCESS, "test", "test"), HttpStatus.OK);
+    }
 }
