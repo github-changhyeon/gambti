335f0925fea7b2a4c5e1144d25f6ea00bf45e219
박수빈 <nnp2329@naver.com>
2021-03-15 17:25:42 +0900

    S04P22B201-46 / feat: 게임 데이터 정제

--- data/products_preprocess.py
+++ data/products_preprocess.py
@@ -1,10 +1,87 @@
 import pandas as pd
+import json
+import os
+import shutil
+import numpy as np
+import pickle
 
-#.jl 파일을 DataFrame으로
-df = pd.read_json('products_all.jl', lines=True)
+DATA_FILE = os.path.join("products_all.jl")
+DUMP_FILE = os.path.join("game_data.pkl")
+
+game_columns = (
+    "id",  # 스팀 고유 게임 아이디
+    "app_name",  # 게임 이름
+    "developer",  # 개발사
+    "publisher",  # 배급사
+    "release_date",  # 출시일
+    "metascore",  # 메타스코어
+    "price",  # 가격(int)
+    "sentiment",  # 사용자 반응
+    "url",  # 게임 링크(스팀링크)
+)
+
+def import_data(data_path=DATA_FILE):
+    """
+    파일을 읽어서 Pandas DataFrame 형태로 저장
+    """
+    try:
+        data = pd.read_json(data_path, lines=True, encoding="utf-8")
+    except FileNotFoundError as e:
+        print(f"`{data_path}` 가 존재하지 않습니다.")
+        exit(1)
+
+    # 결측치 제거 --> id가 존재하지 않는 것은 삭제
+    nullcnt = data["id"].isnull().sum()
+    if nullcnt > 0:
+        data = data.dropna(subset=["id"])
+
+    # app_name이 존재하지 않는 값 삭제
+    data = data.dropna(subset=["app_name"])
+   
+    # id가 float64 형태이기 때문에 index 사용을 위해서 int64 형태로 변환
+    data["id"] = data["id"].astype(np.int64)
+
+    # price 원화를 int로 변환하기 위해 특수문자 제거
+    data["price"] = data["price"].str.replace(pat=r'[^\w]', repl=r'', regex=True)
+    # 숫자인 것은 숫자로 변경하고 아닌 것은 NaN 반환
+    data["price"] = pd.to_numeric(data["price"], errors='coerce')
+
+    #  metascore nan인 것은 0점으로 처리, price 없는 것 0으로 처리
+    data = data.fillna({"metascore" : 0, "price" : 0})
+
+    games = data[[*game_columns, "tags", "specs"]]  #게임관련 데이터 테이블 + tag, specs
+
+    return {"games": games}
 
 
+def dump_dataframes(dataframes):
+    pd.to_pickle(dataframes, DUMP_FILE)
 
 
-# print(tabulate(df.head(), headers='keys', tablefmt='psql'))
-print('total '+ str(len(df)))
+def load_dataframes():
+    return pd.read_pickle(DUMP_FILE)
+
+
+def main():
+
+    print("[*] Parsing data...")
+    data = import_data()
+    print("[+] Done")
+
+    print("[*] Dumping data...")
+    dump_dataframes(data)
+    print("[+] Done\n")
+
+    data = load_dataframes()
+
+    term_w = shutil.get_terminal_size()[0] - 1
+    separater = "-" * term_w
+
+    print("[게임]")
+    print(f"{separater}\n")
+    print(data["games"].head())
+    print(f"\n{separater}\n\n")
+
+
+if __name__ == "__main__":
+    main()
