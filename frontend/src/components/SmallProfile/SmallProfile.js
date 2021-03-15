f827dfbc0ecae84ca3b46f6b7a4a7f8ee6cd984f
백민주 <baiktte@gmail.com>
2021-03-15 15:46:14 +0900

    S04P22B201-59 / feat: profile 제작

--- frontend/src/components/SmallProfile/SmallProfile.js
+++ frontend/src/components/SmallProfile/SmallProfile.js
@@ -1,6 +1,17 @@
-import React from 'react';
+import React, { useState, useEffect } from "react";
 import styles from './SmallProfile.module.css';
+import AvatarComp from 'src/components/AvatarComp/AvatarComp';
+import Typography from "@material-ui/core/Typography";
 
 export default function SmallProfile() {
-  return <h1>SmallProfile Component</h1>;
+  const [nickName, setNickName] = React.useState("김싸피");
+  const [email, setEmail] = React.useState("ssafy@naver.com");
+
+  return (
+    <div className={styles.root}>
+      <AvatarComp size='xsmall'></AvatarComp>
+      <Typography className={styles.nick}>{nickName}</Typography>
+      <Typography className={styles.email}>{email}</Typography>
+    </div>
+  );
 }
