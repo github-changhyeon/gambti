d7074679729963fcb933bfc0c79306bf960da1fc
이동규 <uykgnod@naver.com>
2021-03-16 18:39:55 +0900

    S04P22B201-82 S04P22B201-62 / feat : Game, Image 엔티티 추가

--- backend/src/main/java/com/ssafy/gambti/domain/game/Game.java
+++ backend/src/main/java/com/ssafy/gambti/domain/game/Game.java
@@ -1,4 +1,46 @@
 package com.ssafy.gambti.domain.game;
 
+import com.ssafy.gambti.domain.image.Image;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@Setter
 public class Game {
+
+    @Id
+    @Column(name = "GAME_ID", insertable = false)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String appName;
+
+    private String developer;
+
+    private String publisher;
+
+    private String releaseDate;
+
+    private Long metaScore;
+
+    private Long price;
+
+    private String sentiment;
+
+    private String url;
+
+    private String videoUrl;
+
+    @OneToOne
+    @JoinColumn(name="IMAGE_ID", insertable=false, updatable=false)
+    private Image logoImage;
+
+    @OneToOne
+    @JoinColumn(name="IMAGE_ID", insertable=false, updatable=false)
+    private Image backgroundImage;
+
 }
--- backend/src/main/java/com/ssafy/gambti/domain/image/Image.java
+++ backend/src/main/java/com/ssafy/gambti/domain/image/Image.java
@@ -1,4 +1,20 @@
 package com.ssafy.gambti.domain.image;
 
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@Setter
 public class Image {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="IMAGE_ID")
+    private Long id;
+
+    private String path;
+
 }
