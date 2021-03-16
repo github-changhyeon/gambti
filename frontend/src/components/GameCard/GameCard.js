832bdedf25d0ef52eb7eeefa346bdc319edfd507
Kim Changhyeon <ckdhyeon95@naver.com>
2021-03-16 09:08:08 +0900

    S04P22B201-71 / feat: 게임 카드 공통 컴포넌트 개발

--- frontend/src/components/GameCard/GameCard.js
+++ frontend/src/components/GameCard/GameCard.js
@@ -1,6 +1,69 @@
-import React from 'react';
 import styles from './GameCard.module.css';
+import AvatarComp from 'src/components/AvatarComp/AvatarComp.js';
+import React from 'react';
+import Card from '@material-ui/core/Card';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import Typography from '@material-ui/core/Typography';
+import ButtonComp from 'src/components/ButtonComp/ButtonComp.js';
+import Avatar from '@material-ui/core/Avatar';
 
-export default function GameCard() {
-  return <h1>GameCard Component</h1>;
+export default function GameCard({ isLogin, gameInfo }) {
+  let descriptionNum, descriptionText, buttonText;
+  const cardColor = { color: '#ffffff' };
+  if (isLogin) {
+    descriptionNum = Number(gameInfo.totalJoin).toLocaleString();
+    descriptionText = ' joined';
+  } else {
+    descriptionNum = gameInfo.suitedRate + '%';
+    descriptionText = ' suited';
+  }
+  if (gameInfo.isJoin) {
+    buttonText = 'Join Game';
+  } else {
+    buttonText = 'Joined';
+  }
+
+  return (
+    <Card className={styles.game_card}>
+      <CardMedia
+        className={styles.game_card_background_img}
+        image="https://m.gjcdn.net/community-header/950/18067-crop0_296_1920_776-npqpqk9f-v4.webp"
+        title="Contemplative Reptile"
+      />
+      <CardContent className={styles.card_logo_img}>
+        <AvatarComp size="xlarge" textvalue="temp" />
+      </CardContent>
+      <CardContent>
+        <Typography
+          className={styles.game_card_title}
+          gutterBottom
+          variant="h5"
+          component="h2"
+        >
+          {gameInfo.appName}
+        </Typography>
+        <Typography
+          variant="body1"
+          color="textSecondary"
+          component="span"
+          style={cardColor}
+        >
+          {descriptionNum}
+        </Typography>
+        <Typography
+          variant="body2"
+          color="textSecondary"
+          component="span"
+          style={cardColor}
+        >
+          {descriptionText}
+        </Typography>
+      </CardContent>
+      <CardActions className={styles.game_card_button}>
+        <ButtonComp size="medium" textvalue={buttonText}></ButtonComp>
+      </CardActions>
+    </Card>
+  );
 }
