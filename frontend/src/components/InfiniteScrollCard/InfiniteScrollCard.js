973aab876a2444e7544721d5c0ba02816615119e
Kim Changhyeon <ckdhyeon95@naver.com>
2021-03-17 22:30:36 +0900

    S04P22B201-80 / feat: 무한스크롤링 공통 컴포넌트 개발 - 진행중

--- frontend/src/components/InfiniteScrollCard/InfiniteScrollCard.js
+++ frontend/src/components/InfiniteScrollCard/InfiniteScrollCard.js
@@ -1,6 +1,45 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import styles from './InfiniteScrollCard.module.css';
+import InfiniteScroll from 'react-infinite-scroll-component';
 
-export default function Header() {
-  return <h1>무한 스크롤링</h1>;
+export default function InfiniteScrollCard() {
+  // 새로운 state 변수를 선언하고, count라 부르겠습니다.
+
+  // const [items, setItems] = useState(Array.from({ length: 20 }));
+  const [items, setItems] = useState(new Array());
+  const [pageNum, setPageNum] = useState(1);
+  const [size, setSize] = useState(20);
+  const [isEnd, setIsEnd] = useState(false);
+
+  const fetchData = () => {
+    //TODO: change
+    setTimeout(() => {
+      axios
+        .get(`https://api.openbrewerydb.org/breweries?page=${pageNum}&per_page=${size}`)
+        .then((res) => {
+          //updating data
+          setItems([...items, ...res.data]);
+          //updating page numbers
+          setPageNum(pageNum + 1);
+        });
+    }, 1000);
+  };
+
+  return (
+    <div style={{ height: '300px' }}>
+      <InfiniteScroll
+        dataLength={items.length} //This is important field to render the next data
+        next={fetchData}
+        hasMore={!isEnd}
+        loader={<h4>Loading...</h4>}
+      >
+        {items.map((brewery) => (
+          <ul key={brewery.name}>
+            <li>Name: {brewery.name}</li>
+          </ul>
+        ))}
+      </InfiniteScroll>
+    </div>
+  );
 }
