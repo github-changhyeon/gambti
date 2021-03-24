c0222a725602758c99effaf4d2a65290085b94b2
김예슬 <loveys1023@naver.com>
2021-03-24 19:15:24 +0900

    S04P23B201-36 / feat: Nav 완료

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import { StylesProvider } from '@material-ui/core/styles';
 import Header from 'src/components/Header/Header';
+import Nav from 'src/components/Nav/Nav';
 import Footer from 'src/components/Footer/Footer';
 import routerInfo from 'src/constants/routerInfo';
 import 'src/firebaseConfig';
@@ -25,21 +26,24 @@ function App() {
     <StylesProvider injectFirst>
       <div className="App">
         <Header />
-        <Router>
-          <Switch>
-            <Route exact path={routerInfo.PAGE_URLS.HOME} component={Home} />
-            <Route path={routerInfo.PAGE_URLS.LOGIN} component={Login} />
-            <Route path={routerInfo.PAGE_URLS.SIGNUP} component={Signup} />
-            <Route exact path={routerInfo.PAGE_URLS.FORGOT} component={Forgot} />
-            <Route path={routerInfo.PAGE_URLS.FORGOT_SENT} component={ForgotSent} />
-            <Route path={routerInfo.PAGE_URLS.GAMES} component={GenreGames} />
-            <Route path={routerInfo.PAGE_URLS.SEARCH} component={Search} />
-            <Route path={routerInfo.PAGE_URLS.DETAIL} component={Detail} />
-            <Route exact path={routerInfo.PAGE_URLS.PROFILE} component={Profile} />
-            <Route path={routerInfo.PAGE_URLS.PROFILE_EDIT} component={EditProfile} />
-            <Route path="/test" component={Test} />
-          </Switch>
-        </Router>
+        <Nav />
+        <div style={{ paddingLeft: '70px' }}>
+          <Router>
+            <Switch>
+              <Route exact path={routerInfo.PAGE_URLS.HOME} component={Home} />
+              <Route path={routerInfo.PAGE_URLS.LOGIN} component={Login} />
+              <Route path={routerInfo.PAGE_URLS.SIGNUP} component={Signup} />
+              <Route exact path={routerInfo.PAGE_URLS.FORGOT} component={Forgot} />
+              <Route path={routerInfo.PAGE_URLS.FORGOT_SENT} component={ForgotSent} />
+              <Route path={routerInfo.PAGE_URLS.GAMES} component={GenreGames} />
+              <Route path={routerInfo.PAGE_URLS.SEARCH} component={Search} />
+              <Route path={routerInfo.PAGE_URLS.DETAIL} component={Detail} />
+              <Route exact path={routerInfo.PAGE_URLS.PROFILE} component={Profile} />
+              <Route path={routerInfo.PAGE_URLS.PROFILE_EDIT} component={EditProfile} />
+              <Route path="/test" component={Test} />
+            </Switch>
+          </Router>
+        </div>
         <Footer />
       </div>
     </StylesProvider>
