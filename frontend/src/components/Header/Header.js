28fdb4152460aad534b02e507a458102a8989c23
김예슬 <loveys1023@naver.com>
2021-03-24 23:48:15 +0900

    S04P22B201-84 / feat: Header 로고 클릭시 홈으로 추가

--- frontend/src/components/Header/Header.js
+++ frontend/src/components/Header/Header.js
@@ -4,14 +4,24 @@ import InputBase from '@material-ui/core/InputBase';
 import SearchIcon from '@material-ui/icons/Search';
 import NotificationsIcon from '@material-ui/icons/Notifications';
 import AvatarComp from 'src/components/AvatarComp/AvatarComp';
+import { useHistory } from 'react-router';
+import routerInfo from 'src/constants/routerInfo';
 
 export default function Header() {
+  let history = useHistory();
   const [atextvalue, setAtextvalue] = React.useState('김');
 
   return (
     <div className={styles.header}>
       <div className={styles.header_left}>
-        <img src="/images/gambti_logo.png" alt="logo" style={{ height: '28px' }} />
+        <img
+          className={styles.header_logo}
+          src="/images/gambti_logo.png"
+          alt="logo"
+          onClick={() => {
+            history.push(routerInfo.PAGE_URLS.HOME);
+          }}
+        />
       </div>
       <div className={styles.header_center}>
         <div className={styles.search_icon}>
