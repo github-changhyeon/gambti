2b586c11424ab646c933bb3e58a310fc25058021
백민주 <baiktte@gmail.com>
2021-03-24 19:13:27 +0900

    S04P23B201-71 / feat: axios 토큰

--- frontend/src/Context/UserContext.js
+++ frontend/src/Context/UserContext.js
@@ -1,30 +1,69 @@
-import React, { useState, createContext, useEffect } from "react";
+import React, { useState, createContext, useEffect, useRef } from "react";
 import fire from 'src/fire';
 
 const UserContext = createContext();
 
 const UserProvider = (props) => {
-  const [user, setUser] = useState(null)
+  const [user, setUser] = useState({ isLoggedIn: null });
+  const unsubscribe = useRef();
+
 
   const subscribeUser = (uid) => {
     if (!uid) return;
-    return fire.db.collection("user").doc(uid).onSnapshot((userDoc) => {
-      console.log({ userDoc, ...userDoc.data() })
-    }); //return type : function (unsubscribe user)
+    console.log("subscribing user:", uid);
+
+    const unsub = fire.db
+      .collection("users")
+      .doc(uid)
+      .onSnapshot({
+        next: (user) => {
+          updateUser({ uid: user.id, ...user.data() });
+        },
+        error: (error) => {
+          console.log(`[User Listener Error] ${error.message}`);
+        },
+      });
+    unsubscribe.current = unsub;
   }
+  const unsubscribeCurrentUser = () => {
+    if (!unsubscribe.current) return;
+    console.log("unsubscribing current user...");
+    unsubscribe.current();
+    unsubscribe.current = undefined;
+    console.log("unsubscribed.");
+  };
+
+  const initUser = async (currentUser) => {
+    console.log("initializing user:", currentUser.uid);
+    subscribeUser(currentUser.uid);
+  };
+
+  const updateUser = async (user) => {
+    if (!user.uid) return;
+    setUser({ isLoggedIn: true, ...user });
+    console.log("updated user");
+  };
+
 
   useEffect(() => {
-    fire.auth.onAuthStateChanged((user) => {
-      if (user) {
-        // 로그인 O
-        setUser(user)
-        console.log(user)
-      } else {
-        // 로그인 X
-        setUser(null)
+    const unsubscribeAuth = fire.auth.onAuthStateChanged((currentUser) => {
+      unsubscribeCurrentUser();
+      if (currentUser) {
+        initUser(currentUser);
+        // console.log(currentUser);
       }
+      else {
+        setUser({ isLoggedIn: false });
+      };
     });
-  }, [user]);
+
+    return () => {
+      unsubscribeAuth();
+      unsubscribeCurrentUser();
+    };
+  }, []);
+
+
 
 
   return (
