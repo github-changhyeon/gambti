625c9490b3b3602e1a86fb84d193dc6b93597dd0
박수빈 <nnp2329@naver.com>
2021-03-25 17:05:10 +0900

    S04P23B201-87 / feat: logo 이미지 데이터 수집, 저장

--- data/game_asset_crawling.py
+++ data/game_asset_crawling.py
@@ -4,6 +4,7 @@ import pymysql          #mariaDB 사용
 import sqlalchemy      #DB 접속 엔진
 import json
 import pandas as pd
+import os
 
 # 일단 game의 id와 url을 가져온다
 # url을 하나씩 순회하면서 bs4를 이용하여 대표 이미지 주소 가져오기
@@ -17,6 +18,8 @@ PW = config['DATABASE']['PW']
 DATABASE_URL = 'mysql+pymysql://'+USER+':'+PW+'@'+HOST+':3306/gambti?charset=utf8mb4'
 engine_mariadb = sqlalchemy.create_engine(DATABASE_URL, echo=False)
 
+DUMP_FILE = os.path.join("image_data.pkl")
+
 def get_data():
     # mariadb의 table 값을 읽어서 DataFrame으로 반환
     game_data = pd.read_sql_table('game', engine_mariadb, columns={'game_id', 'url'})
@@ -24,8 +27,24 @@ def get_data():
     return game_data
 
 def save_mariadb(data):
-    #TODO : 이걸로 하면 insert into여서 update를 할 수 있는 것을 찾아야함
-    data.to_sql(name='game', con=engine_mariadb, index=False, if_exists='append') 
+    table_name = 'game'
+    for i in range(len(data)):
+        row = data.iloc[i, :]
+        game_id = row['game_id']
+        path = row['logo_image_path']
+        #UPDATE game SET logo_image_path = 'image_path' WHERE game_id = 1;
+        sql = 'UPDATE {} SET {} = {} WHERE {} = {}'.format(table_name, 'logo_image_path', "'"+path+"'", 'game_id', game_id)
+
+        with engine_mariadb.begin() as conn:
+            conn.execute(sql)
+    
+    print("[*] 데이터 저장 완료")
+
+def dump_dataframes(data):
+    pd.to_pickle(data, DUMP_FILE)
+
+def load_dataframes():
+    return pd.read_pickle(DUMP_FILE)
 
 def image_scrapy(data):
     #url, game_id DataFrame을 순회
@@ -38,14 +57,15 @@ def image_scrapy(data):
         with urlopen(url) as response:
             soup = bs(response, 'html.parser')
             for anchor in soup.select("img.game_header_image_full"):
-                img_url = anchor.get('src')
+                image_url = anchor.get('src')
                 print(i)
 
-        rows_list.append({"game_id":game_id, "logo_image_path":url})
+        rows_list.append({"game_id":game_id, "logo_image_path":image_url})
 
-    #TODO: json 파일로 생성하기
-    #app_name은 not null이다. 
     df = pd.DataFrame(rows_list)
+    
+    #pkl 파일로 생성
+    dump_dataframes(df)
     
     print("스크래핑 끝!")
     save_mariadb(df)
@@ -55,7 +75,10 @@ def main():
     game_data = get_data()
     print("[*] done..")
     print("[+] url 크롤링")
-    image_scrapy(game_data)
+    # image_scrapy(game_data)
+
+    data = load_dataframes()
+    # save_mariadb(data)
     print("[*] success")
 
 if __name__ == "__main__":
