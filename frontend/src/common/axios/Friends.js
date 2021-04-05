935efe480d63777e563f202a7c424cbaa6d7c2f9
백민주 <baiktte@gmail.com>
2021-04-05 18:47:36 +0900

    S04P23B201-165 / feat: 친구 추가 성공, 추천 친구 목록 받기 성공

--- frontend/src/common/axios/Friends.js
+++ frontend/src/common/axios/Friends.js
@@ -3,15 +3,26 @@ import { restApi, getConfig } from "./index";
 // 대략 이런식으로 -> 수정 필요하면 수정해서 사용
 
 // 친구추가
-function addFriend(userId, param, success, fail) {
+function addFriend(toUserId, fromUserToken, success, fail) {
   // param : 계정 정보가 담긴 object
-  const config = getConfig(userId);
+  const config = getConfig(fromUserToken);
   const instance = restApi();
-  instance.post(`/friends/${userId}`, param, config).then(success).catch(fail);
+  instance.post(`/friends/${toUserId}`, {}, config)
+    .then(success)
+    .catch(fail);
+}
+
+function getRecommendedFriends(userToken, success, fail) {
+  // console.log('token', userToken);
+  const config = getConfig(userToken);
+  const instance = restApi();
+  instance.get(`/friends/recommends`, config)
+    .then(success)
+    .catch(fail);
 }
 
 
 
 // 등등
 
-export { addFriend };
+export { addFriend, getRecommendedFriends };
