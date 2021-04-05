34aa2558385a7fd87d3b2d3fc5da4ea51acf2377
박수빈 <nnp2329@naver.com>
2021-04-05 04:36:12 +0000

    S04P23B201-131 / feat: mbti, steam 장르별 점수 계산

---
+++ spark/gambti_recomm.scala
@@ -0,0 +1,63 @@
+import java.util.Properties
+import org.apache.spark.sql._
+import spark.implicits._
+
+object Gambti {
+	case class Genre(mgenre_id: Int, mgenre: String, sgenre_id: Int)
+	case class mGenre(mgenre_id: Int, mbti: String, score: Double)
+
+
+	val url = "jdbc:mysql://gambtidb.c4kbbredlqua.ap-northeast-2.rds.amazonaws.com:3306/gambti?serverTimezone=UTC&useUnicode=true&characterEncoding=utf8"
+	val prop = new Properties()
+	
+	def loadMariaDB(){
+		prop.put("user", "ssafy")
+		prop.put("password", "gambti123!")
+		prop.setProperty("driver", "com.mysql.cj.jdbc.Driver")
+		print("[*] Load MariaDB Success")
+	}
+	
+	def getTable(tname: String): DataFrame = {
+		val get_df = spark.read.jdbc(url, tname, prop)
+		
+		return get_df
+	}
+	
+	def calcMbtiRating(user: DataFrame, game: DataFrame): DataFrame = {
+		val genre_df = getTable("genre")
+		val game_genre_df = getTable("game_genre")
+	
+		//전처리 : 계산에 필요한 정보만 추출
+		val user_info = user.select('no, 'age, 'gender, 'max_price, 'mbti)
+		val game_info = game.select('game_id, 'metascore)
+		
+		//Rating 계산할 정보
+		val raw_data = spark.read.textFile("/home/ubuntu/apps/spark/data/gambti/mbti_steam_genre.txt")
+		val genre_mapping_df = raw_data.map(x => x.split("\t")).map(x => Genre(x(0).toInt, x(1), x(2).toInt)).toDF()
+
+		val raw_data2 = spark.read.textFile("/home/ubuntu/apps/spark/data/gambti/mbti_mgenre_rating.txt")
+		val mgenre_rating_df = raw_data2.map(x => x.split("\t")).map(x => mGenre(x(0).toInt, x(1), x(2).toDouble)).toDF()
+		
+		//계산에 사용할 count 구하기
+		val sgenre_count = genre_mapping_df.groupBy("sgenre_id").count.orderBy("sgenre_id")
+
+		val join_genre_rating = mgenre_rating_df.join(genre_mapping_df, Seq("mgenre_id"), "inner").orderBy("mbti","mgenre_id")
+		val mbti_genre_score = join_genre_rating.groupBy("mbti", "sgenre_id").agg(sum("score"))
+		val join_count = mbti_genre_score.join(sgenre_count, Seq("sgenre_id"), "inner").orderBy("mbti", "sgenre_id").withColumnRenamed("sum(score)", "score")
+
+		val mbti_genre_rating = join_count.withColumn("rating", 'score/'count)
+		val final_mgenre_rating = mbti_genre_rating.drop('score).drop('count)
+		
+		return final_mgenre_rating
+	}	
+	
+
+	def main(args: Array[String]){
+		loadMariaDB()
+
+		val user_df = getTable("user")
+		val game_df = getTable("game")
+		
+		val mbti_rating_df = calcMbtiRating(user_df, game_df)
+	}
+}
