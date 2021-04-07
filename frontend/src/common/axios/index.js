0e9140556bd1e0c245b41edf40dd7f3b9c769907
이동규 <uykgnod@naver.com>
2021-04-08 03:24:24 +0900

    S04P23B201-155 / fix: 토큰 만료 10분 전 요청 시 갱신으로 수정

--- frontend/src/common/axios/index.js
+++ frontend/src/common/axios/index.js
@@ -10,36 +10,41 @@ function restApi() {
   });
 }
 
-function checkTokenExpiration() {
+async function checkTokenExpiration() {
   // 현재 로그인한 유저가 있는지 가져온다
-  var currentUser = fire.auth.currentUser;
+  var currentUser = fire.auth.currentUser
   
   // 로그인한 유저가 있다면
   if (currentUser) {
     // 로그인한 유저의 토큰정보를 가져와서
-    currentUser.getIdTokenResult()
+    await currentUser.getIdTokenResult()
     .then(res => {
-      // 토큰의 만료시간과 현재시간을 비교해서 만료시간이 지났다면 로그아웃 시킨다.
-      if (new Date(res.expirationTime).getTime() - new Date().getTime() < 0) {
-        fire.auth.signOut()
-        .then(() => {
-          window.localStorage.clear();
-        // TODO: 홈으로 이동   
+      // 토큰 만료시간이 10분 전이라면 토큰을 재발급해준다.
+      if (new Date(res.expirationTime).getTime() - new Date().getTime() < 600000) {
+        currentUser.getIdToken(true)
+        .then(res => {
+          localStorage.setItem("idToken", res)
         })
         .catch(err => {
-          // An error happened.
-        });
+          alert(err)
+        }); 
       }
     })
     .catch(err => {
       alert(err)
     })
+  } else {
+    // 로컬스토리지에 토큰을 비운다.
+    localStorage.clear()
+    window.location.reload()
   }
 }
 
 function getConfig(token) {
-  // 현재 토큰이 유요한 토큰인지 확인 한 후 만료된 토큰이면 로그아웃 하는 함수
-  checkTokenExpiration()
+  // 현재 로컬 스토리지에 토큰을 가지고 있는 유저면 토큰의 유효성 검사를 위해 확인한다
+  if (localStorage.getItem("idToken")) {
+    checkTokenExpiration()
+  }
 
   const config = {
     headers: {
@@ -48,6 +53,9 @@ function getConfig(token) {
     },
   };
   return config;
+}
+export default function AxiosIndex() {
+  
 }
 
 export { restApi, getConfig };
